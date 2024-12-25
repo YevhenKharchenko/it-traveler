a4ef1ee27119773025726a9c28420f486d0aaecb
YevhenKharchenko <evgenvalerych@gmail.com>
2024-12-25 21:31:27 +0200

    Add Homepage

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,11 @@
-<template>Hello world</template>
+<script setup>
+import HomepageView from './views/HomepageView.vue'
+import FavoritePlaces from './components/FavoritePlaces/FavoritePlaces.vue'
+</script>
+
+<template>
+  <HomepageView />
+  <div class="bg-white h-screen w-[400px]">
+    <FavoritePlaces />
+  </div>
+</template>
